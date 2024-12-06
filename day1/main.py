97abae6c40456fba09963f836b75d648bd770a03
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2024-12-06 13:12:36 +0100

    add day1

---
+++ day1/main.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import numpy as np
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+    from numpy.typing import NDArray
+
+fname: str | Path = "input.txt"
+data = np.loadtxt(fname, dtype=np.int32)  # shape of (1000, 2)
+
+
+# %% part 1
+def get_distances(data: NDArray[np.int32]) -> int:
+    """Get the total distances."""
+    distances = np.abs(np.sort(data[:, 1]) - np.sort(data[:, 0]))
+    return np.sum(distances)
+
+
+total = get_distances(data)
+print(f"The total distance is {total}.")
+
+
+# %% part 2
+def get_similarity(data: NDArray[np.int32]) -> int:
+    """Get the similarity score."""
+    intersection = np.intersect1d(data[:, 0], data[:, 1])
+    total = 0
+    for elt in intersection:
+        total += np.where(data[:, 1] == elt)[0].size * elt
+    return total
+
+
+total = get_similarity(data)
+print(f"The total similarity score is {total}.")
