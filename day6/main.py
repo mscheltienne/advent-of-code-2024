319b4d13546da45875da05a248917edf5f9f5f83
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2024-12-06 20:08:56 +0100

    add example and basics for a graph/loop approach

--- day6/main.py
+++ day6/main.py
@@ -4,6 +4,7 @@ from itertools import cycle
 from pathlib import Path
 from typing import TYPE_CHECKING
 
+import networkx as nx
 import numpy as np
 
 if TYPE_CHECKING:
@@ -11,7 +12,7 @@ if TYPE_CHECKING:
 
     from numpy.typing import NDArray
 
-fname: Path = Path(__file__).parent / "input.txt"
+fname: Path = Path(__file__).parent / "example.txt"
 char_to_num: dict[str, int] = {".": 0, "#": 1, "^": 2, ">": 3, "v": 4, "<": 5}
 with open(fname) as fid:
     data: list[list[int]] = [
@@ -33,26 +34,26 @@ _DIRECTIONS: dict[int, tuple[int, int]] = {
     4: (1, 0),
     5: (0, -1),
 }
-_DIRECTIONS_CYCLE: Generator = cycle(_DIRECTIONS)
 
 
 def walk_pattern(
     data: NDArray[np.bool], x: int, y: int, direction: int
 ) -> set[tuple[int, int]]:
     """Walk the guard pattern and list the positions visited."""
-    while next(_DIRECTIONS_CYCLE) != direction:
+    direction_cycle: Generator = cycle(_DIRECTIONS)
+    while next(direction_cycle) != direction:
         pass
     positions = {(x, y)}
     while True:
         dx, dy = _DIRECTIONS[direction]
-        nx, ny = x + dx, y + dy  # next position
-        if nx < 0 or data.shape[0] <= nx or ny < 0 or data.shape[1] <= ny:
-            break
-        elif data[nx, ny]:
-            direction = next(_DIRECTIONS_CYCLE)
+        nextx, nexty = x + dx, y + dy  # next position
+        if nextx < 0 or data.shape[0] <= nextx or nexty < 0 or data.shape[1] <= nexty:
+            break  # out of the map
+        elif data[nextx, nexty]:
+            direction = next(direction_cycle)
             continue
         # move forward and record the guard position
-        x, y = nx, ny
+        x, y = nextx, nexty
         positions.add((x, y))
     return positions
 
@@ -60,4 +61,31 @@ def walk_pattern(
 positions = walk_pattern(data, x, y, orientation)
 print(f"The guard walked on {len(positions)} positions.")
 
+
 # %% part 2
+def build_graph(data: NDArray[np.bool], x: int, y: int, direction: int) -> nx.DiGraph:
+    """Create a graph reprensenting the guard pattern.
+
+    Each node represents a (x, y, orientation) tuple.
+    Each edge represents a forward movement of the guard.
+    """
+    direction_cycle: Generator = cycle(_DIRECTIONS)
+    while next(direction_cycle) != direction:
+        pass
+    G = nx.DiGraph()
+    G.add_node((x, y, direction))
+    while True:
+        dx, dy = _DIRECTIONS[direction]
+        nextx, nexty = x + dx, y + dy  # next position
+        if nextx < 0 or data.shape[0] <= nextx or nexty < 0 or data.shape[1] <= nexty:
+            break  # out of the map
+        elif data[nextx, nexty]:
+            direction = next(direction_cycle)
+            continue
+        # move forward and add the node (x, y, o) and its edges
+        x, y = nextx, nexty
+        stop = (x, y, direction) in G
+        G.add_edge((x - dx, y - dy, direction), (x, y, direction))
+        if stop:
+            break
+    return G
