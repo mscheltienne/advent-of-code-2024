93d66348cc7074ba8039c3eae3c796cc92e35ae5
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2024-12-06 16:07:11 +0100

    fix bounds for day4

--- day4/main.py
+++ day4/main.py
@@ -15,17 +15,19 @@ with open(fname) as fid:
 data = np.array([list(elt.strip()) for elt in data])
 
 # %% part 1
+# fmt: off
 _DIRECTIONS: list[tuple[int, int]] = [
-    (0, 1),  # going right
-    (0, -1),  # going left
-    (1, 0),  # going down
-    (-1, 0),  # going up
-    (1, 1),  # going down-right
-    (1, -1),  # going down-left
-    (-1, 1),  # going up-right
-    (-1, -1),  # going up-left
+    (0, 1),   # right
+    (0, -1),  # left
+    (1, 0),   # down
+    (-1, 0),  # up
+    (1, 1),   # down-right
+    (1, -1),  # down-left
+    (-1, 1),  # up-right
+    (-1, -1), # up-left
 ]
 _WORD: str = "XMAS"
+# fmt: on
 
 
 def get_number_of_word(data: NDArray) -> int:
@@ -42,14 +44,15 @@ def _count_around_first_letter(data: NDArray, i: int, j: int) -> int:
     """Count the number of patterns at that X location."""
     count = 0
     for offset in _DIRECTIONS:
-        try:
-            for k, letter in enumerate(_WORD):
-                if data[i + k * offset[0], j + k * offset[1]] != letter:
-                    break
-            else:
-                count += 1
-        except IndexError:  # out of bounds
+        if not 0 <= i + (len(_WORD) - 1) * offset[0] < data.shape[0]:
             continue
+        if not 0 <= j + (len(_WORD) - 1) * offset[1] < data.shape[1]:
+            continue
+        for k, letter in enumerate(_WORD):
+            if data[i + k * offset[0], j + k * offset[1]] != letter:
+                break
+        else:
+            count += 1
     return count
 
 
