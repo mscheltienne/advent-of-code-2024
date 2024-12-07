056c2ffc608298bb088cc1c83aa6ac8494107c16
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2024-12-07 14:53:43 +0100

    update day6

--- day6/main.py
+++ day6/main.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
 from itertools import cycle
 from pathlib import Path
 from typing import TYPE_CHECKING
@@ -28,7 +29,7 @@ data[x, y] = 0
 data: NDArray[np.bool] = data.astype(np.bool)
 
 # %% part 1
-_DIRECTIONS: dict[int, tuple[int, int]] = {
+_MOVEMENTS: dict[int, tuple[int, int]] = {
     2: (-1, 0),
     3: (0, 1),
     4: (1, 0),
@@ -36,21 +37,21 @@ _DIRECTIONS: dict[int, tuple[int, int]] = {
 }
 
 
-def walk_pattern(
-    data: NDArray[np.bool], x: int, y: int, direction: int
+def walk_path(
+    data: NDArray[np.bool], x: int, y: int, orientation: int
 ) -> set[tuple[int, int]]:
     """Walk the guard pattern and list the positions visited."""
-    direction_cycle: Generator = cycle(_DIRECTIONS)
-    while next(direction_cycle) != direction:
+    orientation_cycle: Generator = cycle(_MOVEMENTS)
+    while next(orientation_cycle) != orientation:
         pass
     positions = {(x, y)}
     while True:
-        dx, dy = _DIRECTIONS[direction]
+        dx, dy = _MOVEMENTS[orientation]
         nextx, nexty = x + dx, y + dy  # next position
         if nextx < 0 or data.shape[0] <= nextx or nexty < 0 or data.shape[1] <= nexty:
             break  # out of the map
         elif data[nextx, nexty]:
-            direction = next(direction_cycle)
+            orientation = next(orientation_cycle)
             continue
         # move forward and record the guard position
         x, y = nextx, nexty
@@ -58,34 +59,111 @@ def walk_pattern(
     return positions
 
 
-positions = walk_pattern(data, x, y, orientation)
+positions = walk_path(data, x, y, orientation)
 print(f"The guard walked on {len(positions)} positions.")
 
 
 # %% part 2
-def build_graph(data: NDArray[np.bool], x: int, y: int, direction: int) -> nx.DiGraph:
+@dataclass(frozen=True)
+class Node:
+    """A node representing the guard position/orientation and the map state."""
+
+    x: int
+    y: int
+    orientation: int
+    x_obstacle: int | None
+    y_obstacle: int | None
+
+
+def turn_right(orientation: int) -> int:
+    """Turn the guard to the right."""
+    return {2: 3, 3: 4, 4: 5, 5: 2}[orientation]
+
+
+def in_bounds(data: NDArray[np.bool_], x: int, y: int) -> bool:
+    """Check if the guard is still on the map."""
+    rows, cols = data.shape
+    return 0 <= x < rows and 0 <= y < cols
+
+
+def can_turn_right(data: NDArray[np.bool], x: int, y: int, orientation: int) -> bool:
+    """Check if the guard can turn right."""
+    dx, dy = _MOVEMENTS[turn_right(orientation)]
+    nextx, nexty = x + dx, y + dy
+    if not in_bounds(data, nextx, nexty):
+        return False
+    return not data[nextx, nexty]
+
+
+def walk_path(
+    G: nx.DiGraph,
+    data: NDArray[np.bool],
+    x: int,
+    y: int,
+    orientation: int,
+    x_obstacle: int,
+    y_obstacle: int,
+) -> None:
+    """Walk down a path and add the corresponding nodes to the graph."""
+    for _ in range(2):  # find the next position
+        dx, dy = _MOVEMENTS[orientation]
+        nextx, nexty = x + dx, y + dy
+        if not in_bounds(data, nextx, nexty):
+            return
+        if data[nextx, nexty]:  # obstacle in the next location
+            new_orientation = turn_right(orientation)
+            G.add_edge(
+                Node(x, y, orientation, x_obstacle, y_obstacle),
+                Node(x, y, new_orientation, x_obstacle, y_obstacle),
+            )
+            orientation = new_orientation
+            continue
+        break
+    next_node = Node(nextx, nexty, orientation, x_obstacle, y_obstacle)
+    if next_node not in G:
+        G.add_edge(Node(x, y, orientation, x_obstacle, y_obstacle), next_node)
+        walk_path(G, data, nextx, nexty, orientation, x_obstacle, y_obstacle)
+
+
+def build_graph(data: NDArray[np.bool], x: int, y: int, orientation: int) -> nx.DiGraph:
     """Create a graph reprensenting the guard pattern.
 
     Each node represents a (x, y, orientation) tuple.
     Each edge represents a forward movement of the guard.
     """
-    direction_cycle: Generator = cycle(_DIRECTIONS)
-    while next(direction_cycle) != direction:
-        pass
     G = nx.DiGraph()
-    G.add_node((x, y, direction))
+    G.add_node(Node(x, y, orientation, None, None))  # starting position
+    # walk down the normal path, and at every step branch out between the normal path
+    # and the one where the guard turns right due to a new obstacle in front of her.
     while True:
-        dx, dy = _DIRECTIONS[direction]
+        dx, dy = _MOVEMENTS[orientation]
         nextx, nexty = x + dx, y + dy  # next position
-        if nextx < 0 or data.shape[0] <= nextx or nexty < 0 or data.shape[1] <= nexty:
-            break  # out of the map
-        elif data[nextx, nexty]:
-            direction = next(direction_cycle)
-            continue
-        # move forward and add the node (x, y, o) and its edges
-        x, y = nextx, nexty
-        stop = (x, y, direction) in G
-        G.add_edge((x - dx, y - dy, direction), (x, y, direction))
-        if stop:
+        if not in_bounds(data, nextx, nexty):
             break
+        elif data[nextx, nexty]:
+            new_orientation = turn_right(orientation)
+            G.add_edge(
+                Node(x, y, orientation, None, None),
+                Node(x, y, new_orientation, None, None),
+            )
+            orientation = new_orientation
+            continue
+        # move forward and add the node (x, y, o) corresponding to the normal path
+        G.add_edge(
+            Node(x, y, orientation, None, None),
+            Node(nextx, nexty, orientation, None, None),
+        )
+        # check if the guard can turn right, in which case let's try to add an obstacle
+        # in front of her to branch out.
+        if can_turn_right(data, x, y, orientation):
+            data[nextx, nexty] = True
+            walk_path(G, data, x, y, orientation, nextx, nexty)
+            data[nextx, nexty] = False
+        x, y = nextx, nexty
     return G
+
+
+G = build_graph(data, x, y, orientation)
+nx.draw(G)
+cycles = nx.simple_cycles(G)
+print(f"The guard walked on {len(list(cycles))} cycles.")
